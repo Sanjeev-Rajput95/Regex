c41374a36c10f4c8806ac4975baefc04393a526d
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-20 22:43:49 +0530

    Check last alphabet or special character should not

--- Regex/Regex/Program.cs
+++ Regex/Regex/Program.cs
@@ -17,6 +17,12 @@ namespace RegexPrograms
              bool alphabet_SpecialChar = rg.IsMatch("A400088");
              Console.WriteLine("Alphabet : " + alphabet_SpecialChar);
 
+             string pattern1 = @"^[1-9]{6}[A-Z]{7}$";
+             
+             Regex rgobj = new Regex(pattern1);
+             bool result1 = rgobj.IsMatch("400088B");
+             Console.WriteLine("Check not last alphabet " + result1);
+
 
 
         }
