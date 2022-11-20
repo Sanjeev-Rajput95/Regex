77e2422d5fb680794bb6fe50874ed4e37d871a77
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-20 21:11:20 +0530

    Create a regex pattern to validate PIN code 400088

---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============== WELCOME TO REGEX PROGRAM ==============");
+
+            string pattern = @"^[1-9]{1}[0-9]{5}";
+
+             Regex rg = new Regex(pattern);
+             bool result = rg.IsMatch("400088");
+             Console.WriteLine(result);
+
+        }
+    }
+}
